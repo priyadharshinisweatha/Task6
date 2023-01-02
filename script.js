71c51d6acacf109d6741e50cb0cc0c194bc2af45
priyadharshinisweatha <120706191+priyadharshinisweatha@users.noreply.github.com>
2023-01-01 23:27:26 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+var RESUME = {
+    "PERSONAL PROFILE" :{
+        "NAME" : "PRIYADHARSHINI.A",
+        "DATE OF BIRTH": "08/10/1999",
+        "FATHER NAME":"ANBARASU.M",
+        "MOTHER NAME":"AMALESHWARI.A",
+        "SEX" : "FEMALE",
+        "REGISTOR ID":"171121101023",
+        "EMAIL ID" : "priyaswe8@gmail.com",
+        "PHONE NO" : "9600135996",
+         // "REGISTOR NO" : "171121101023",
+         "ADDRESS":"NO.1,THAMBU SAMY NAGAR,2'ND STREET ,THIRUVERKADU,CHENNAI-600077"
+        },
+        "EDUCATION QUALIFICATIONS" : { 
+            "SSLC" : "70.6%", 
+            "YEAROFPASSING":2015,
+            "HSC" : "59.3%",
+            "YEAR OF PASSING":2017,
+            "B.TECH" : "82.8%",
+            "YEAROFPASSING":2021
+
+        },
+        "CERTIFICATE ":{
+            "PROGRAMMING LANGUAGE": "JAVA ORACLE"
+
+        },
+        
+        "SKILLS":{
+            "TECHICAL SKILLS":["BASIC KNOWLEDGE IN  C++","JAVA","MYSQL",
+                                "PROFICIENT IN WINDOWS"]
+        },
+            
+            " GENERAL" : {
+                s1:"POSITIVE THINKING",
+                        s2: "ABILITY TO QUICKLY UNDERSTAND",
+                         s3:"SMART WORKER",
+                         s4:"GOOD WRITING  AND LISTENING SKILLS"
+        },             
+        "TRAINING & WORKSHOP" :{
+            W1:"COMPLETED TRAINGING ON SQL OFFERED BY ORACLE IN MGR UNIVERSITY",
+            W2:"COMPLETED TRAINING ON JAVA AT DR MGR EDUCATIONAL AND RESEARCH UNIVERSITY",
+            W3:'COMPLETED INPLANT TRAINING ON ANDROID TECHNOLOGY AT SIGNWARE TECHNOLOGY'
+
+        },
+
+            "HOBBIES" :[
+                "LISTENING MUSIC", "READING BOOKS" ,"DANCING"         ],
+
+            "DECLARATION" :{
+                "DECLARATION":" I HEREBY DECLARE THAT ALL INFORMATION FURNISHEDS ABOVE IS TRUE TO THE BEST OF MY KNOWELEGE",
+            "PLACE": "CHENNAI",
+            "DATE": "01/01/2023"},
+
+    }
+console.table(RESUME["PERSONAL PROFILE"])
+console.table(RESUME["EDUCATION QUALIFICATIONS"])
+console.table(RESUME["SKILLS"])
+console.log(RESUME["CERTIFICATE "])
+console.log(RESUME["TRAINING & WORKSHOP"])
+console.log(RESUME["HOBBIES"])
+console.log(RESUME["DECLARATION"])
+
+// // var head1=[ 
+// //     {
+        
+// //            careerobjective : "to be a part o fo company that indulges professional growth which provides challenging and rewaarding career whilw allowing me to utilize my knowledge and skills.",
+// //     },
+// //     {
+// //     summary :"i am good fun and frendly person, i am honest and punctual. i ahve good communication skill. i have creative mind and am always realy for new challenges. i can deal with difficulti situations. maybe a lot of people are more the good then me, but with perseverance and strong will can defeat all."
+// //     },
+// //     {
+// //         personalskills:"vreativity, interpersonal skills, critical thinking, problem solving, public speaking, customer service skills, teamwork skill, communication."
+// //     }
+// //     ]
+// //      var second =[
+// //     {
+// //         Qualification:'Bsc.computer science',
+// //         Schoolcollege:'Sridevi arts and science college',
+// //         Board:'University of medras',
+// //         yearofpassing:'2022',
+// //         percentageCGPA:'85%'
+// //     },
+// //     {
+// //         Qualification:'HSC',
+// //         Schoolcollege:'KBC GOVT GIRLS HR SEC school redhils',
+// //         Board:'State board',
+// //         yearofpassing:'2019',
+// //         percentageCGPA:'48%'
+// //     },
+// //     {
+// //         Qualification:'SSLC',
+// //         Schoolcollege:'govt high school n.e.v. palayam, thiruvallur',
+// //         Board:'State board',
+// //         yearofpassing:'2017',
+// //         percentageCGPA:'78%'
+// //     }
+// //      ]
+    
+    //  var skills=
+    //     ["c","c++","HTML5", "CSS3", "JAVASCRIPT", "RDBMS,", "SQL", "ms office"]
+    // var personalinfo={
+    // name:"sankari",
+    // father:"N.MUrugan",
+    // DOB:"02-01-2002",
+    // Gender:"Female",
+    // MAritalstatus:"single",
+    // Nationality:"indian",
+    // Religion:"HIndu",
+    // Mobileno: 7824804291
+    // }
+    // console.log(skills)
